8f738d0250d73b388a29a87f2e831bda0b1b009e
alinghi <alinghi@kaist.ac.kr>
2019-09-04 21:57:42 +0900

    Implement init function - I/O

--- chicken.py
+++ chicken.py
@@ -10,6 +10,22 @@ sys.stdout=open("output.txt","w")
 def init():
     N,M=map(int,input().split())
     l=[list(map(int,input().split())) for _ in range(N)]
-    return N,M,l
+    home,chicken=[],[]
+    for i in range(N):
+        for j in range(N):
+            if l[i][j]==1:
+                home.append((i,j))
+            if l[i][j]==2:
+                chicken.append((i,j))
+    return N,M,l, home, chicken
+def dist(a,b):
+    return abs(a[0]-b[0])+abs(a[1]-b[1])
+def nearest_chicken(a,chicken):
+    #when list of chicken zip given
+    di=150
+    for i in chicken:
+        di=min(di,dist(a,i))
+    return di
 
-N,M,l = init()
+N,M,l,home, chicken = init()
+print(home)
