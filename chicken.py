d6edc2886433f36b76a801fc3263f3b379a8ce8b
alinghi <alinghi@kaist.ac.kr>
2019-09-04 22:30:18 +0900

    Implement init function - I/O

--- chicken.py
+++ chicken.py
@@ -1,6 +1,6 @@
 '''
 https://www.acmicpc.net/problem/15686
-Chicken Delivery Problme
+Chicken Delivery Problem
 Author: alinghi
 '''
 import sys
@@ -18,14 +18,30 @@ def init():
             if l[i][j]==2:
                 chicken.append((i,j))
     return N,M,l, home, chicken
+
 def dist(a,b):
     return abs(a[0]-b[0])+abs(a[1]-b[1])
+
 def nearest_chicken(a,chicken):
     #when list of chicken zip given
     di=150
     for i in chicken:
         di=min(di,dist(a,i))
     return di
+def solver(cnt):
+    if len(chicken_candidate)==M:
+        ret=0
+        for a in home:
+            ret+=nearest_chicken(a,chicken_candidate)
+        return ret
+    if cnt>=len(chicken):
+        return 987654321
+    chicken_candidate.append(chicken[cnt])
+    r1=solver(cnt+1)
+    chicken_candidate.pop()
+    r2 = solver(cnt + 1)
+    return min(r1,r2)
 
+chicken_candidate=[]
 N,M,l,home, chicken = init()
-print(home)
+print(solver(0))
